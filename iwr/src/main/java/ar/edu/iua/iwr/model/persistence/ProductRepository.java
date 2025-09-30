b6212440a5b881b84ee2e1d89b23f54fd4e273f2
joaquincernik <joaquincernik@gmail.com>
2025-09-30 18:12:44 -0300

    pequenios

--- iwr/src/main/java/ar/edu/iua/iwr/model/persistence/ProductRepository.java
+++ iwr/src/main/java/ar/edu/iua/iwr/model/persistence/ProductRepository.java
@@ -3,9 +3,12 @@ package ar.edu.iua.iwr.model.persistence;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.iua.iwr.model.Product;
+import jakarta.transaction.Transactional;
 
 @Repository
 //<cual es el tipo de entidad, primary key?
@@ -17,4 +20,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	//spring lee el nombre del metodo y parse siguiendo palabras clave: findBy, and, not
 	//y despues lo traduce
 	Optional<Product> findByProductAndIdNot(String product,long id);
+	
+	//en vez de trabajar por conuslta por nombre escribimos nuestra propia consulta
+	@Query(value = "SELECT count(*) FROM products where id_category=?", nativeQuery = true) //constula personalizada
+	public Integer countProductsByCategory(long idCategory);
+	
+	@Transactional
+    @Modifying //es la que indica que los datos se van a modificar
+    @Query(value = "UPDATE products SET stock=? WHERE id=?", nativeQuery = true) //establece el valor de stock para un producto  en particular
+	public int setStock(boolean stock, long idProduct);
+
 }
